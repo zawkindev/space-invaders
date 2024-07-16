d1c204ab91fbac531d83ff72d3562c8c02e26dd3
zawkindev <zawkindev@gmail.com>
2024-07-16 10:08:28 +0500

    delete all

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	s "space-invaders/screen"
 )
 
 func main() {
-	helloText := s.Model{Cordinate: s.Position.Center, Content: "hello\tworld\nbye world!"}
 
-	// for {
-	// 	time.Sleep(16 * time.Millisecond) // 60 fps = 1:60
-	// }
-	s.Render(helloText)
 }
